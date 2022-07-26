f177f18cddea7e2d8f70c98fdc646f91e5b76112
TunnRKA <96030186+TunnARK@users.noreply.github.com>
2022-07-26 16:32:32 +0200

    Hello World Test

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+print("Hello World\n")
+
+print("--> How to code with Python on Atom\n")
+
+print("\t 1. Create .py file\n")
+
+print("\t 2. Open Terminal with Alt+Shift+T\n or")
+print("\t 3. Run the code with Ctlr+Shift+B\n")
